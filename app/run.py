5fc93c82304f5d1e569e2bbea13d7b474fb931eb
Juan <sjuanandres0@gmail.com>
2021-04-06 08:49:03 +0200

    Update host and port

--- app/run.py
+++ app/run.py
@@ -94,7 +94,8 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+#    app.run(host='0.0.0.0', port=3001, debug=True)
+    app.run(host='127.0.0.1', port=4000, debug=True)
 
 
 if __name__ == '__main__':
